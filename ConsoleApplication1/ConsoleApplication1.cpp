7580f97edb774217b6c211acfd36cd9afca5cfbf
Tim Godisberg <timothy.lindberg01@gmail.com>
2020-11-09 12:51:16 +0100

    lagt till hello world loopen för alla sätt, do while for. även gjort så att man skriver in värdet man vill den ska loopa.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,16 +4,47 @@
 #include <iostream>
 using namespace std;
 
-void helloWorldFunc(int x) 
+void helloWorldFunc() 
 {
+	int x;
+	cout << "Hur många loopar vill du köra?: ";
+	cin >> x;
 	for (int i = 0; i < x; i++)
 	{
 		cout << "Hello World!\n";
 	}
 }
 
+void helloWorldWhile()
+{
+	int i = 0;
+	int x;
+	cout << "Hur många loopar vill du köra?: ";
+	cin >> x;
+	while (x > i) 
+	{
+		cout << "Hello World (While)\n";
+		i = i + 1;
+	}
+}
+
+void helloWorldDo()
+{	
+	int x;
+	int i = 0;
+	cout << "Hur många loopar vill du köra?: ";		
+	cin >> x;	//input värdet blir till x
+	do {
+		cout << "Hello World (do)\n";
+		i = i + 1;
+	} 
+	while (i < x);
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
-	helloWorldFunc(5);
+    //anropar på functionerna
+	helloWorldFunc();
+	helloWorldWhile();
+	helloWorldDo();
 }
